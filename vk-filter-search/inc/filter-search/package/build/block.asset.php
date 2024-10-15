3342dea6f2d9257c6064f34f025c75cf81c847ad
Sean Murphy <sean@iamseanmurphy.com>
2024-10-15 15:01:49 +0000

    2.13.0.1

--- vk-filter-search/inc/filter-search/package/build/block.asset.php
+++ vk-filter-search/inc/filter-search/package/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-server-side-render'), 'version' => '224c282854aee72428b0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-server-side-render'), 'version' => '957633ff5cdc805c918e');
